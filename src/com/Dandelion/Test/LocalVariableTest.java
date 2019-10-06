18c4e3997bab20aceab735dc548a42740c54fd5f
Dandelion <1670787983@qq.com>
2019-10-06 22:22:25 +0800

    2222

---
+++ src/com/Dandelion/Test/LocalVariableTest.java
@@ -0,0 +1,9 @@
+package com.Dandelion.Test;
+
+public class LocalVariableTest {
+
+    public static void main(String[] args) {
+        int a = 0;//局部变量必须赋初始值才能正常使用
+        System.out.println(a);
+    }
+}
